e12a3a5b71d3c218783830d9776a35db97054d82
banebizic <bane.bizic@gmail.com>
2024-10-11 16:20:56 +0200

    Added list of movies to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, HttpClientModule, NgIf, NgFor,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+// export class HomeComponent implements OnInit {
 
+//   private client: HttpClient
+//   public recomended: any[] = []
+
+//   constructor(private httpClient: HttpClient){
+//     this.client = httpClient
+//   }
+//   ngOnInit(): void {
+//     const url ='assets/data/spisakFilmova.json'
+//     this.client.get<any>(url, {
+//       headers: {
+//         'Accept': 'application/json'
+//       }
+//     }).subscribe(rsp => this.recomended = rsp.content)
+//   }
+
+// }
+export class HomeComponent implements OnInit {
+
+  public recomended: any[] = []; // Niz koji će sadržati učitane filmove
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    const url = 'assets/data/spisakFilmova.json'; // Putanja do JSON fajla
+    this.httpClient.get<any[]>(url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).subscribe(
+      (response) => {
+        this.recomended = response; // Učitani filmovi
+      },
+      (error) => {
+        console.error('Greška prilikom učitavanja filmova', error);
+      }
+    );
+  }
 }
