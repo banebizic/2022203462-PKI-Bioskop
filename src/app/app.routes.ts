077f9a15ea77b5b6df7fac19638bffab80374d97
banebizic <bane.bizic@gmail.com>
2024-10-12 23:18:33 +0200

    Male izmene na home, i dodao rutu za Cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'cart', component: CartComponent },
     { path: '**', redirectTo: '' }
 ];
